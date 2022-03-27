6fc0f648fa5a6534ba7fca2ade74eb8a811fa454
Davi Augusto <droidcenter0377@gmail.com>
2022-03-27 16:10:30 -0300

    atualizacao...

--- Classes e Metodos/classe_ContasBanco.py
+++ Classes e Metodos/classe_ContasBanco.py
@@ -7,6 +7,7 @@ import pytz
 import time
 from random import randint
 
+
 class ContaCorrente():
     '''
     Cria um objeto ContaCorrente para gerenciar as contas dos clientes
@@ -44,7 +45,8 @@ class ContaCorrente():
             Exibe o saldo atual da conta do cliente
             Não há parâmetros necessários.
         '''
-        print('{}, seu saldo atual é de R$ {:,.2f}'.format(self._nome, self.__saldo))
+        print('{}, seu saldo atual é de R$ {:,.2f}'.format(
+            self._nome, self.__saldo))
 
     def depositar_dinheiro(self, valor):
         self.__saldo += valor
@@ -90,18 +92,20 @@ class ContaCorrente():
 
 
 class CartaoCredito:
-    
+
     @staticmethod
     def _data_hora():
         fuso_BR = pytz.timezone('Brazil/East')
         horario_BR = datetime.now(fuso_BR)
         return horario_BR
-    
+
     def __init__(self, titular, conta_corrente):
         self.numero = randint(1000000000000000, 9999999999999999)
         self.titular = titular
-        self.validade = '{}/{}'.format(CartaoCredito._data_hora().month, CartaoCredito._data_hora().year + 4)
-        self.cod_seguranca = '{}{}{}'.format(randint(0, 9), randint(0, 9), randint(0, 9))
+        self.validade = '{}/{}'.format(CartaoCredito._data_hora().month,
+                                       CartaoCredito._data_hora().year + 4)
+        self.cod_seguranca = '{}{}{}'.format(
+            randint(0, 9), randint(0, 9), randint(0, 9))
         self.limite = 1000
         self._senha = '1234'
         self.conta_corrente = conta_corrente
@@ -110,103 +114,10 @@ class CartaoCredito:
     @property
     def senha(self):
         return self._senha
-    
+
     @senha.setter
     def senha(self, valor):
-        if len(valor) ==4 and valor.isnumeric():
+        if len(valor) == 4 and valor.isnumeric():
             self._senha = valor
         else:
             print("Nova senha inválida!")
-
-
-# programa
-conta_davi = ContaCorrente("Davi", "111.222.333-45", "3025", "505257")
-conta_davi.consultar__saldo()
-
-conta_nilva = ContaCorrente("Nilva", "222.333.444-55", "5555", "656565")
-conta_drica = ContaCorrente("Adriana", "555.666.777-89", "5030", "757575" )
-
-#depositar um dinheirinho na conta:
-deposito = 25000
-print('\n --- >>> Depositando R$ {:,.2f}\n'.format(deposito))
-conta_davi.depositar_dinheiro(deposito)
-conta_drica.depositar_dinheiro(deposito)
-conta_nilva.depositar_dinheiro(deposito)
-print('-----------------')
-
-# # Sacando um dinheirinho na conta:
-# saque = 100000
-# print('\n --- >>>  Sacando R$ {:,.2f}\n'.format(saque))
-# conta_davi.sacar_dinheiro(saque)
-# # conta_davi.consultar__saldo()
-# print('-----------------')
-
-
-# Transferir dinheiro pra mae
-# transferencia = 1500
-# print('\n --- >>>  Transferindo R$ {:,.2f}\n'.format(transferencia))
-# conta_davi.transferir(transferencia, conta_maeDavi)
-# conta_davi.consultar__saldo()
-# print('-----------------')
-
-# Mostrando Saldo e Limite de Conta
-# print(' --- >>> Mostrando saldo Final: ')
-# conta_davi.consultar__saldo()
-# conta_davi.consultar_limite_chequeespecial()
-
-# print('==' * 28)
-# print(conta_davi.transacoes)
-# conta_davi.consultar_historico_transacoes()
-# print('==' * 28)
-# print('\n')
-
-
-print('consultar Historico de {}'.format(conta_nilva._nome))
-conta_nilva.consultar_historico_transacoes()
-print('==' * 28)
-
-
-print('\n')
-print('consultar Historico de {}'.format(conta_drica._nome))
-conta_drica.consultar_historico_transacoes()
-print('==' * 28)
-print('\n')
-
-# Documentação da Classe
-# help(ContaCorrente)
-
-cartao_davi = CartaoCredito('Davi', conta_davi)
-
-print('==' * 11 + ' Cartão de Crédito  ========')
-print('==' * 25)
-print('Titular: {}'.format(cartao_davi.titular))
-print('Cartao: {}'.format(cartao_davi.numero))
-print('Validade: {}  PIN: {}'.format(cartao_davi.validade, cartao_davi.cod_seguranca))
-print('Limite do cartão: R$ {:,.2f}'.format(cartao_davi.limite))
-print('==' * 25)
-print('\n')
-
-cartao_drica = CartaoCredito('Adriana', conta_drica)
-
-print('==' * 11 + ' Cartão de Crédito  ========')
-print('==' * 25)
-print('Titular: {}'.format(cartao_drica.titular))
-print('Cartao: {}'.format(cartao_drica.numero))
-print('Validade: {}  PIN: {}'.format(
-    cartao_drica.validade, cartao_drica.cod_seguranca))
-print('Limite do cartão: R$ {:,.2f}'.format(cartao_drica.limite))
-print('==' * 25)
-
-# getter e setter
-print('=======----------->>> Getter e Setter')
-print('==' * 25)
-cartao_davi.senha = '12'
-print(cartao_davi.senha)
-
-conta_davi.nome = "Davi Augusto"
-print(conta_davi.nome)
-print('Nova Senha:')
-cartao_davi.senha = '2345'
-print(cartao_davi.senha)
-print('---------------')
-
---
+++ Classes e Metodos/main_Banco.py
@@ -0,0 +1,97 @@
+from classe_ContasBanco import ContaCorrente, CartaoCredito, Conta
+
+# programa
+conta_davi = ContaCorrente("Davi", "111.222.333-45", "3025", "505257")
+conta_davi.consultar__saldo()
+
+conta_nilva = ContaCorrente("Nilva", "222.333.444-55", "5555", "656565")
+conta_drica = ContaCorrente("Adriana", "555.666.777-89", "5030", "757575")
+
+#depositar um dinheirinho na conta:
+deposito = 25000
+print('\n --- >>> Depositando R$ {:,.2f}\n'.format(deposito))
+conta_davi.depositar_dinheiro(deposito)
+conta_drica.depositar_dinheiro(deposito)
+conta_nilva.depositar_dinheiro(deposito)
+print('-----------------')
+
+# # Sacando um dinheirinho na conta:
+# saque = 100000
+# print('\n --- >>>  Sacando R$ {:,.2f}\n'.format(saque))
+# conta_davi.sacar_dinheiro(saque)
+# # conta_davi.consultar__saldo()
+# print('-----------------')
+
+
+# Transferir dinheiro pra mae
+# transferencia = 1500
+# print('\n --- >>>  Transferindo R$ {:,.2f}\n'.format(transferencia))
+# conta_davi.transferir(transferencia, conta_maeDavi)
+# conta_davi.consultar__saldo()
+# print('-----------------')
+
+# Mostrando Saldo e Limite de Conta
+# print(' --- >>> Mostrando saldo Final: ')
+# conta_davi.consultar__saldo()
+# conta_davi.consultar_limite_chequeespecial()
+
+# print('==' * 28)
+# print(conta_davi.transacoes)
+# conta_davi.consultar_historico_transacoes()
+# print('==' * 28)
+# print('\n')
+
+
+print('consultar Historico de {}'.format(conta_nilva._nome))
+conta_nilva.consultar_historico_transacoes()
+print('==' * 28)
+
+
+print('\n')
+print('consultar Historico de {}'.format(conta_drica._nome))
+conta_drica.consultar_historico_transacoes()
+print('==' * 28)
+print('\n')
+
+# Documentação da Classe
+# help(ContaCorrente)
+
+cartao_davi = CartaoCredito('Davi', conta_davi)
+
+print('==' * 11 + ' Cartão de Crédito  ========')
+print('==' * 25)
+print('Titular: {}'.format(cartao_davi.titular))
+print('Cartao: {}'.format(cartao_davi.numero))
+print('Validade: {}  PIN: {}'.format(
+    cartao_davi.validade, cartao_davi.cod_seguranca))
+print('Limite do cartão: R$ {:,.2f}'.format(cartao_davi.limite))
+print('==' * 25)
+print('\n')
+
+cartao_drica = CartaoCredito('Adriana', conta_drica)
+
+print('==' * 11 + ' Cartão de Crédito  ========')
+print('==' * 25)
+print('Titular: {}'.format(cartao_drica.titular))
+print('Cartao: {}'.format(cartao_drica.numero))
+print('Validade: {}  PIN: {}'.format(
+    cartao_drica.validade, cartao_drica.cod_seguranca))
+print('Limite do cartão: R$ {:,.2f}'.format(cartao_drica.limite))
+print('==' * 25)
+
+# getter e setter
+print('=======----------->>> Getter e Setter')
+print('==' * 25)
+cartao_davi.senha = '12'
+print(cartao_davi.senha)
+
+conta_davi.nome = "Davi Augusto"
+print(conta_davi.nome)
+print('Nova Senha:')
+cartao_davi.senha = '2345'
+print(cartao_davi.senha)
+print('---------------\n')
+
+print('--------- Todos Atributos ---------------')
+print(conta_davi.__dict__)
+print(cartao_davi.__dict__)
