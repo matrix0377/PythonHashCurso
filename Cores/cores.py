8ff120ba7435c924f3dd66f2a75cfbb925e5920b
Davi Augusto <droidcenter0377@gmail.com>
2022-04-25 23:37:31 -0300

    atualizacao

--- Cores/cores.py
+++ Cores/cores.py
@@ -17,7 +17,7 @@ def cores(*args):
             'pretoebranco': '\033[7;30m'}
     return cor.items()
 '''
-    
+
 cor = {'limpa': '\033[m',
        'azul': '\033[34m',
        'amarelo': '\033[33m',
@@ -29,14 +29,19 @@ cor = {'limpa': '\033[m',
        'pretoevermelho': '\033[7;31;41[m',
        'pretoebranco': '\033[7;30m'}
 
-                      
-nome = 'Davi'
-idade = 44
-#cor = cores(cor.item())
 
-print("Olá! Muito prazer em conhecer, {} {} : {} {}!!!".format(cor['verde'], nome, idade, cor['limpa']))
-print("Olá! Muito prazer em conhecer, {} {} : {} {}!!!".format(cor['lilas'], nome, idade, cor['limpa']))
-print("Olá! Muito prazer em conhecer, {} {} : {} {}!!!".format(cor['cian'], nome, idade, cor['limpa']))
+if __name__ == "__main__":
+
+    nome = 'Davi'
+    idade = 44
+    #cor = cores(cor.item())
+
+    print("Olá! Muito prazer em conhecer, {} {} : {} {}!!!".format(
+        cor['verde'], nome, idade, cor['limpa']))
+    print("Olá! Muito prazer em conhecer, {} {} : {} {}!!!".format(
+        cor['lilas'], nome, idade, cor['limpa']))
+    print("Olá! Muito prazer em conhecer, {} {} : {} {}!!!".format(
+        cor['cian'], nome, idade, cor['limpa']))
 
 
 # as cores fora da função funcionam bem!
--- Exerc_ListComprehension/List Comprehension e Functions.py
+++ Exerc_ListComprehension/List Comprehension e Functions.py
@@ -1,22 +1,44 @@
 '''
     List Comprehension e Functions
 '''
+import os
+import sys
+sys.path.insert(0, '/PythonHashCurso/Cores/')
+
+from Cores import cores
+
+# cor = {'limpa': '\033[m',
+#        'azul': '\033[34m',
+#        'amarelo': '\033[33m',
+#        'verde': '\033[32m',
+#        'lilas': '\033[35m',
+#        'branco': '\033[37m',
+#        'vermelho': '\033[31m',
+#        'cian': '\033[36m',
+#        'pretoevermelho': '\033[7;31;41[m',
+#        'pretoebranco': '\033[7;30m'}
+
+os.system('cls')
+
 produtos = ['apple tv', 'mac', 'IPhone x', 'IPhone 11', 'IPad', 'apple watch', 'mac book', 'airpods']
-produtos.sort()
-print('----- Utilizando sort -------------\n')
+print(cor['lilas'], '---------- Print Lista Original -------------',cor['limpa'], '\n')
 print(produtos)
 
-print('\n----- Utilizando sort -------------\n')
+produtos.sort()
+print('\n ----- Utilizando sort -------------\n')
+print(produtos)
+
+print('\n----- Utilizando sorted -------------\n')
 # sorted cria uma lista nova, precisa de outra variável para atribuir
 lista_nova = sorted(produtos)
 print(lista_nova)
 
-print('\n----- Utilizando sort -------------\n')
+print('\n----- Utilizando sort casefold-------------\n')
 # ordenando na ordem e deixando os texto da forma que estavam escritas
 produtos.sort(key=str.casefold)
 print(produtos)
 
-print('\n----- Ordenando um dicionário por valor -------------\n')
+print('\n----- Ordenando um dicionário por valor ListCompreehension -------------\n')
 vendas_produtos = {'vinho': 100, 'cafeiteira': 150, 'microondas': 300, 'iphone': 5500}
 
 def segundo_item(tupla):
