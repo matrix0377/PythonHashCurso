897934b8d05f7b0d34556695759e4050dd6150e8
Davi Augusto <droidcenter0377@gmail.com>
2022-04-26 00:34:39 -0300

    atualizacao

--- Exerc_ListComprehension/List Comprehension e Functions.py
+++ Exerc_ListComprehension/List Comprehension e Functions.py
@@ -3,20 +3,20 @@
 '''
 import os
 import sys
-sys.path.insert(0, '/PythonHashCurso/Cores/')
+# sys.path.insert(1, '/PythonHashCurso/Cores/')
+# from Cores import *
 
-from Cores import cores
-
-# cor = {'limpa': '\033[m',
-#        'azul': '\033[34m',
-#        'amarelo': '\033[33m',
-#        'verde': '\033[32m',
-#        'lilas': '\033[35m',
-#        'branco': '\033[37m',
-#        'vermelho': '\033[31m',
-#        'cian': '\033[36m',
-#        'pretoevermelho': '\033[7;31;41[m',
-#        'pretoebranco': '\033[7;30m'}
+# Dicionário de cores
+cor = {'limpa': '\033[m',
+       'azul': '\033[34m',
+       'amarelo': '\033[33m',
+       'verde': '\033[32m',
+       'lilas': '\033[35m',
+       'branco': '\033[37m',
+       'vermelho': '\033[31m',
+       'cian': '\033[36m',
+       'pretoevermelho': '\033[7;31;41[m',
+       'pretoebranco': '\033[7;30m'}
 
 os.system('cls')
 
@@ -28,22 +28,29 @@ produtos.sort()
 print('\n ----- Utilizando sort -------------\n')
 print(produtos)
 
-print('\n----- Utilizando sorted -------------\n')
+print(cor['cian'], '\n----------- Utilizando sorted -------------\n',
+      cor['limpa'])
 # sorted cria uma lista nova, precisa de outra variável para atribuir
 lista_nova = sorted(produtos)
 print(lista_nova)
 
-print('\n----- Utilizando sort casefold-------------\n')
+print(cor['azul'], '\n----------- Utilizando sort casefold ----------------\n', cor['limpa'])
 # ordenando na ordem e deixando os texto da forma que estavam escritas
 produtos.sort(key=str.casefold)
 print(produtos)
 
-print('\n----- Ordenando um dicionário por valor ListCompreehension -------------\n')
+print(cor['amarelo'], '\n\n----->>> Ordenando um dicionário por valor ListCompreehension <<<-------------\n', cor['limpa'])
 vendas_produtos = {'vinho': 100, 'cafeiteira': 150, 'microondas': 300, 'iphone': 5500}
+print(cor['vermelho'], '\n ---------- Original ----------------\n', cor['limpa'])
+print(vendas_produtos)
+
 
 def segundo_item(tupla):
     return tupla[1]
 lista_vendas = list(vendas_produtos.items())
 lista_vendas.sort(key=segundo_item, reverse=True)
+print(cor['verde'], '\n ---------- dicionario ordenado por vendas ----------------\n', cor['limpa'])
 print(dict(lista_vendas))
+print('\n')
+
 
