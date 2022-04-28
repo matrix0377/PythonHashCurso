c6fd4e9d709ad84efad0640c321f023ffbe5beea
Davi Augusto <droidcenter0377@gmail.com>
2022-04-27 23:41:19 -0300

    Acrescentando exercicio

--- Exerc_API/Python_API_01.py
+++ Exerc_API/Python_API_01.py
@@ -5,6 +5,7 @@ API cotação de Moedas
 import requests
 import os
 import json
+import matplotlib.pyplot as plt
 
 
 os.system('cls')
@@ -44,4 +45,15 @@ print(lista_cotacoes_dolar)
 
 print(cor['verde'], '\n Pegar as cotações do BitCoin de Jan/20 a Out/20\n', cor['limpa'])
 
+cotacoes_btc = requests.get('https://economia.awesomeapi.com.br/json/daily/BTC-BRL/200?start_date=20200101&end_date=20201031')
+cotacoes_btc_dic = cotacoes_btc.json()
+lista_cotacoes_btc = [float(item['bid']) for item in cotacoes_btc_dic]
+lista_cotacoes_btc.reverse()
+print(lista_cotacoes_btc)
+print(cor['lilas'], 'Qtde de Cotações', len(lista_cotacoes_btc), cor['limpa'])
 
+print(cor['cian'], '\n Gráfico com as Cotações do BitCoin\n', cor['limpa'])
+
+plt.figure(figsize=(15, 5))
+plt.plot(lista_cotacoes_btc)
+plt.show()
